41c660c0f0c57d5a1cb52236a97ecec7997f7d8c
AndreaGhizzoni <andrea.ghz@gmail.com>
2014-11-17 10:33:46 +0100

    added javadoc on JXTimerComponent

--- src/main/java/it/hackcaffebabe/jxswingplus/timer/exp/JXTimerComponent.java
+++ src/main/java/it/hackcaffebabe/jxswingplus/timer/exp/JXTimerComponent.java
@@ -4,11 +4,20 @@ import javax.swing.*;
 import static it.hackcaffebabe.jxswingplus.timer.exp.JXTimerUtils.*;
 
 /**
- * //TODO add doc
+ * This is a swing component to display the timer from
+ * {@link it.hackcaffebabe.jxswingplus.timer.exp.JXTimer}. This is used as a
+ * simple JLabel, but, instead to use <code>setText("sometext")</code> there is
+ * a package method to display the time on this component.
  */
-public class JXTimerComponent extends JLabel {
+public class JXTimerComponent extends JLabel
+{
     private String formattedTime;
 
+    /**
+     * Instance the JXTimerComponent. By default this will display the string
+     * "00:00:00" if time to start of JXTimer is not set, otherwise the time to
+     * start with the same pattern.
+     */
     public JXTimerComponent(){
         super();
         this.setTime(0L);
